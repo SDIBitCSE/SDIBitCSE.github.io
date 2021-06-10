716ac826d3875a23b394dba4a272065caf3b212e
Sachin Kumar <sachinsingh0400@gmail.com>
2021-06-10 16:02:43 +0530

    Updated Message

--- src/pages/Submitted.js
+++ src/pages/Submitted.js
@@ -6,7 +6,7 @@ function Submitted(){
         <Segment size="massive" color="teal">
             <h2>
                 Registered Successfully. Please pay 20/- using UPI 7597477435 (Akhil) to complete your registration.<br />
-                Please send the screenshot on whatsapp 7597477435.
+                Please send the screenshot on whatsapp 7597477435.<br />
                 Thank You.
             </h2>
         </Segment>
